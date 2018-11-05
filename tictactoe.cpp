8953ba96b31ed9e9fceac17e70ff6fedcba43fc5
Paul Barrett <fastturtle4@gmail.com>
2018-11-05 12:42:50 -0500

    first commit

---
+++ tictactoe.cpp
@@ -0,0 +1,20 @@
+// Paul Barrett
+// Tic-tac-toe
+
+#include <sys/socket.h>
+
+int main(){
+	
+	//create array for positions
+	//create board
+	//find possible win positions or tie game positions
+	//print board updated for each turn
+	//check current positions against win positions each turn
+	//server will make random move from pool of remaining available moves
+	
+	//possible needed functions:
+	//void displayBoard : show the board with updated positions
+	//
+
+
+}
